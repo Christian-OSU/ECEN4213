7f2bdf1f5e95336445c03a0397dcea9f8ddc33bc
Christian-OSU <christian.landrum@okstate.edu>
2025-10-15 18:07:17 -0500

    Read bumper, drop, cliff, and button data from Kobuki

--- Lab4/code/Lab4EX1.cpp
+++ Lab4/code/Lab4EX1.cpp
@@ -1,3 +1,4 @@
+//Christian Landrum Wyatt Probst Group 5
 //Use g++ -std=c++11 -o Lab4EX1 Lab4EX1.cpp -lwiringPi
 
 #include <string>
@@ -37,17 +38,30 @@ int main(){
 
 		/*Read the bytes containing the bumper, wheel drop,
 			and cliff sensors. You can convert them into a usable data type.*/
+		bumper = serialGetchar(kobuki);
+		drop = serialGetchar(kobuki);
+		cliff = serialGetchar(kobuki);
 
 		/*Print the data to the screen.*/
+		cout << "Bumper: " << bumper << ", " << "Drop: " << drop << ", " << "Cliff: " << cliff << endl;
 
 		/*Read through 6 bytes between the cliff sensors and
 		the button sensors.*/
+		serialGetchar(kobuki);
+		serialGetchar(kobuki);
+		serialGetchar(kobuki);
+		serialGetchar(kobuki);
+		serialGetchar(kobuki);
+		serialGetchar(kobuki);
 
 		/*Read the byte containing the button data.*/
-
+		button = serialGetchar(kobuki);
 		/*Close the script and the connection to the Kobuki when
 		Button 1 on the Kobuki is pressed. Use serialClose(kobuki);*/
-
+		if (button == 1) {
+			serialClose(kobuki);
+			exit(0);
+		}
 
 		
 
